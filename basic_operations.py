f74d66bc66d386efc8b9c7277cc2aebbd12b8a52
salahsheded <shededsalah46@gmail.com>
2025-07-14 17:01:47 +0300

    Update basic_operations.py

--- basic_operations.py
+++ basic_operations.py
@@ -1,4 +1,4 @@
-# قم بتعيين قيم للمتغيرين number1 و number2
+# قم بتعيين قيم المتغيرين number1 و number2
 number1 = 10
 number2 = 5
 
@@ -7,7 +7,7 @@ addition_result = number1 + number2
 subtraction_result = number1 - number2
 multiplication_result = number1 * number2
 
-# طباعة النتائج بالتنسيق المطلوب
+# طباعة الناتج بالشكل المطلوب
 print(f"Addition of {number1} and {number2} is {addition_result}")
 print(f"Subtraction of {number1} and {number2} is {subtraction_result}")
 print(f"Multiplication of {number1} and {number2} is {multiplication_result}")
